bf86b30e8631198fdfa99d3bc2a77a7f2f930fe6
sury228 <cloudsuryansh9@gmail.com>
2026-08-05 01:34:07 +0530

    loggingfile and half exceptionfile edit

---
+++ networksecurity/exception/exception.py
@@ -0,0 +1,13 @@
+import sys
+
+class NetworkSecurityException(Exception):
+    def __init__(self, error_message,error_detail:sys):
+        self.error_message = error_message
+        _,_,exc_tb = error_detail.exc_info()
+
+        self.lineno = exc_tb.tb_lineno
+        self.filename = exc_tb.tb_frame.f_code.co_filename
+
+    def __str__(self):
+        return f"Error occured in script: [{self.filename}] at line number: [{self.lineno}] error message: [{self.error_message}]"
+       
