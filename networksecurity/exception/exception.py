4a628814dc2d31ab5affa1bc7e98188e7d4e91fb
sury228 <cloudsuryansh9@gmail.com>
2026-08-05 13:26:48 +0530

    completed the logging file

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,13 +1,29 @@
 import sys
+from networksecurity.logging import logger
 
 class NetworkSecurityException(Exception):
-    def __init__(self, error_message,error_detail:sys):
+    def __init__(self, error_message, error_detail: sys):
         self.error_message = error_message
-        _,_,exc_tb = error_detail.exc_info()
+        _, _, exc_tb = error_detail.exc_info()
 
-        self.lineno = exc_tb.tb_lineno
-        self.filename = exc_tb.tb_frame.f_code.co_filename
+        if exc_tb is not None:
+            self.lineno = exc_tb.tb_lineno
+            self.filename = exc_tb.tb_frame.f_code.co_filename
+        else:
+            self.lineno = None
+            self.filename = None
 
     def __str__(self):
-        return f"Error occured in script: [{self.filename}] at line number: [{self.lineno}] error message: [{self.error_message}]"
-       
+        location = f"[{self.filename}] at line number: [{self.lineno}]" if self.filename is not None else "[unknown location]"
+        return f"Error occurred in script: {location} error message: [{self.error_message}]"
+    
+    
+# if __name__ == "__main__":
+#     try:
+#         logger.info("ENTER THE TRY BLOCK")
+#         a = 1/0
+#         print("this will not be printed", a)
+
+#     except Exception as e:
+#         raise NetworkSecurityException(e, sys) from e
+    
---
+++ networksecurity/logging/__init__.py
@@ -0,0 +1,5 @@
+from .logging import logging as _logging
+
+logger = _logging.getLogger("networksecurity")
+
+__all__ = ["logger"]
